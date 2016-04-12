777ff8bd2d2b162e8de6f59291603b24eec8b50b
DoTriThoai <lol.dtt.lol@gmail.com>
2016-04-12 11:12:50 +0700

    Commit TestController

---
+++ src/main/java/com/trithoai/schoolmanager/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.trithoai.schoolmanager.controller;
+
+/**
+ * Created by johncarter on 12/04/2016.
+ */
+public class TestController {
+    public void test(){
+        System.out.println("Test push to Github");
+    }
+}
